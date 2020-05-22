177f8a217e5ef2e875ff25985e23630f20f015f5
Thor40 <nicholas.kosik@gmail.com>
2020-05-22 10:08:26 -0500

    finish employee object

--- Index.js
+++ Index.js
@@ -10,12 +10,13 @@ function Role() {
 Role.prototype.getRole = function() {
     // console.log(`Please fill out the following prompt to build your team!`);
     inquirer
-        .prompt({
-        type: 'text',
+        .prompt([
+        {
+        type: 'input',
         name: 'manager',
         message: 'What is your team managers name? (Required)',
-            validate: titleInput => {
-                if (titleInput) {
+            validate: manager => {
+                if (manager) {
                     return true;
                 } else {
                     console.log('Please enter your name!');
@@ -24,11 +25,11 @@ Role.prototype.getRole = function() {
             }
         },
         {
-        type: 'text',
+        type: 'input',
         name: 'managerEmail',
         message: 'What is your team managers email? (Required)',
-            validate: titleInput => {
-                if (titleInput) {
+            validate: managerEmail => {
+                if (managerEmail) {
                     return true;
                 } else {
                     console.log('Please enter the email!');
@@ -37,18 +38,19 @@ Role.prototype.getRole = function() {
             }
         },
         {
-        type: 'text',
+        type: 'input',
         name: 'managerOfficeNumber',
         message: 'What is your team managers office number? (Required)',
-            validate: titleInput => {
-                if (titleInput) {
+            validate: managerOfficeNumber => {
+                if (managerOfficeNumber) {
                     return true;
                 } else {
                     console.log('Please enter the office number!');
                     return false
                 }
             }
-        })
+        }
+        ])
         // .then(({name}) => {
         //     this.name = new Manager(name);
         // });
